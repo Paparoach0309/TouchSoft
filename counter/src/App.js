bd1b41270df951919420f6a7139071dcc6e018e4
Paparoach0309 <silek88@mail.ru>
2021-11-21 23:31:51 +0300

    complete react

---
+++ counter/src/App.js
@@ -0,0 +1,32 @@
+import { useState } from 'react';
+import React from 'react';
+import './App.css';
+
+const App = () => {
+	const [count, setCount] = useState(0);
+
+	const decrease = () => {
+		setCount(count-1);
+		if (count < 1) {
+			setCount(0);
+		} 
+	};
+
+    const reset = () => {
+        setCount(0);
+    };
+
+	return (
+		<div className="app">
+			<div className="counter">{count}</div>
+            <div className= "even-number" style={(count % 2 === 0 ? {background:'red'} : {background:'white'})}>Введено четное число</div>
+            <div className="counter-buttons">
+                <button className="counter-btn" onClick={decrease}>-</button>
+                <button className="counter-btn" onClick={reset}>Reset</button>
+                <button className="counter-btn" onClick={()=>setCount(count+1)}>+</button>
+            </div>
+		</div>
+	);
+}
+
+export default App;
